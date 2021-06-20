b631ce2460835a134f6fe7aaa8683a73d03c79cd
first-phoenix <iswarajkhan@gmail.com>
2021-06-20 21:35:21 +0530

    Added skills page and skill tiles

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import FooterLink from '../FooterLink/FooterLink';
+import Header from '../Header/Header';
 import './Skills.css';
+import skillsVector from './../../assets/skills_vector.png'
+import { skillList } from './../../assets/skillsData';
+import SkillCard from './SkillCard';
 
 const Skills = () => {
     return (
         <div className='section-container'>
-            <h1>I am the skills page</h1>
+            < Header
+                heading="My Skills"
+                details="Put Details here"
+            />
+
+            <div className="skill-card-container">
+                {
+                    skillList.map(({ skillName, skillUrl }) => {
+                        // const {skillName, skillUrl}=skill;
+                        // const skillName = skill.skillName;
+                        // const skillUrl = skill.skillUrl;
+                        // return <h1>{skill.skillName}</h1>
+                        return < SkillCard skillName={skillName} skillUrl={skillUrl} />
+                    })
+                }
+            </div>
+
+            < FooterLink
+                phrase="Get in " link="touch." toAdress="/contact"
+            />
+
+            <div className="skills-vector-frame">
+                <img
+                    src={skillsVector}
+                    alt="skills"
+                    className="skills-vector"
+                />
+            </div>
         </div>
     );
 }
