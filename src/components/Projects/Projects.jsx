0740213c6ccdd902247ee52f41ce9da5e314f728
first-phoenix <iswarajkhan@gmail.com>
2021-06-20 22:24:22 +0530

    modified modal and added contact page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import FooterLink from '../FooterLink/FooterLink';
+import Header from '../Header/Header';
 import './Projects.css';
 
 const Projects = () => {
     return (
         <div className='section-container'>
-            <h1>I am the projects page</h1>
+            < Header
+                heading="My projects in short"
+                details="Add lines here"
+
+            />
+
+            < FooterLink
+                phrase="Check out"
+                link="My Skills"
+                toAdress="/skills"
+            />
+            {/* <h1>I am the projects page</h1> */}
         </div>
     );
 }
