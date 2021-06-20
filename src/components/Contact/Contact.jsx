7f5d0d90cf9b039c23d30b906d8a743bdcdec443
first-phoenix <iswarajkhan@gmail.com>
2021-06-21 00:08:21 +0530

    final commit after connecting form and changing package.json

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -19,12 +19,15 @@ const Contact = () => {
 
             {/* form section */}
             <div className='contact-form-container'>
-                <form className='contact-form'>
+                <form
+                    action="https://formspree.io/f/mpzkrnjl"
+                    method="POST"
+                    className='contact-form'>
                     {/* Email Id Input */}
                     <input
                         type='email'
                         placeholder='Your Email ID'
-                        name='email'
+                        name='_replyto'
                         className='input-box email-input'
                         autoComplete='off'
                     />
